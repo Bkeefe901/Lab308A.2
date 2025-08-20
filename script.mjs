aa9b5a5f33149a7772bb26115a8153fc3e02b77d
Bkeefe901 <bradyjokeefe@gmail.com>
2025-08-20 14:35:57 -0400

    First Commit

---
+++ script.mjs
@@ -0,0 +1,12 @@
+// Lab 308A.2.1 An Object-Oriented Adventure
+
+// Instructions
+// Initialize a new git repository in a local project folder and create a JavaScript file to contain your code. Within your code editor of choice, follow along with the steps below to create the adventure game. Feel free to take creative liberty with your own game!
+// Commit frequently! Every time something works, you should commit it. Remember, you can always go back to a previous commit if something breaks.
+
+
+const adventurer = {
+    name: "Robin",
+    health: 10,
+    inventory: ["sword", "potion", "artifact"]
+}
